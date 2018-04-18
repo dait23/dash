17fa4dbc54717462cb762fe0132deb8f6617bed0
Aditya Maulana <dait23@gmail.com>
2018-04-18 11:44:09 +0700

    Update SSL

--- src/views/Api/Api.js
+++ src/views/Api/Api.js
@@ -6,15 +6,15 @@ export const MainApi = 'https://api.graph.cool/simple/v1/cjf2a92f2349p0196n9r5co
 export const NewApi = 'https://api.graph.cool/simple/v1/cjf0sqzbp4qf70176u7wwsa20';
 
 /////Web API LINK
-export const MainLink = 'http://localhost:3000/';
+export const MainLink = 'https://dashboard.spazee.co.id/';
 
 /////Cloudinary
 export const Cloudinary_Name = 'spazeeid';
 export const Cloudinary_Code = 'spazeeid';
 export const Cloudinary_Link = 'https://api.cloudinary.com/v1_1/spazeeid/image/upload';
 
-export const No_Image = 'http://res.cloudinary.com/ringoid/image/upload/c_thumb,h_50,w_50/v1504372500/noavatar_o558wb.png';
-export const No_Avatar = 'http://res.cloudinary.com/spazeeid/image/upload/c_thumb,q_auto:best,w_80/v1516781262/no-avatar_iy4zru.png';
+export const No_Image = 'https://res.cloudinary.com/ringoid/image/upload/c_thumb,h_50,w_50/v1504372500/noavatar_o558wb.png';
+export const No_Avatar = 'https://res.cloudinary.com/spazeeid/image/upload/c_thumb,q_auto:best,w_80/v1516781262/no-avatar_iy4zru.png';
 
 
 
